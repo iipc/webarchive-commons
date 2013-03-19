efe714c76b41c9460fb3bd5c04cf0cf41f984746
Noah Levitt <nlevitt@archive.org>
2013-03-18 20:31:59 -0700

    splitting archive-commons into two projects part 1: ia-web-commons

--- ia-tools/src/main/java/org/archive/hadoop/cdx/BlockLoader.java
+++
@@ -1,8 +0,0 @@
-package org.archive.hadoop.cdx;
-
-import java.io.IOException;
-
-public interface BlockLoader {
-	
-	public byte[] readBlock(String url, long start, int length) throws IOException;
-}

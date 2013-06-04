05ce5666d0f49451d3f783356c686519bec5e04e
Noah Levitt <nlevitt@archive.org>
2013-06-04 15:41:00 -0700

    improve help message

--- src/main/java/org/archive/util/Grep.java
+++ src/main/java/org/archive/util/Grep.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -27,7 +28,7 @@ public class Grep {
 				g.setGroup(Integer.valueOf(arg.substring(2)));
 			} else if (arg.matches("--group=\\d+")) {
 				g.setGroup(Integer.valueOf(arg.substring(8)));
-			} else if (arg.matches("-o")) {
+			} else if (arg.matches("-o") || arg.equals("--only-matching")) {
 				g.setGroup(0);
 			} else if (g.getRegex() == null) {
 				g.setRegex(Pattern.compile(arg)); // throws exception if invalid regex
@@ -41,15 +42,36 @@ public class Grep {
 
 	public static void main(String[] args) throws Exception {
 		if (args.length < 1) {
-			System.out.println("Usage: jgrep [-v] [-kPOS] [-gGROUP] [-o] PATTERN [FILE]...");
+			printHelp(System.err);
 			System.exit(1);
+		} else if (args[0].equals("-h") || args[0].equals("-?") || args[0].equals("--help")) {
+		    printHelp(System.out);
+		    System.exit(0);
 		}
 
 		Grep g = parseArguments(args);
 		g.doTheGrepThing();
 	}
 
-	protected boolean invert;
+	protected static void printHelp(PrintStream out) {
+	    out.println("Usage:");
+	    out.println("  jgrep [-v] [-kPOS] [-gGROUP] [-o] PATTERN [FILE]...");
+	    out.println();
+	    out.println("Sort of like grep(1), but using java regular expressions, and with a couple of");
+	    out.println("unique options.");
+	    out.println();
+	    out.println("Options:");
+	    out.println("  -v, --invert-match    Select non-matching lines");
+	    out.println("  -k, --key=POS         Match the given whitespace-delimited field, somewhat");
+	    out.println("                        analogous to sort -k");
+	    out.println("  -g, --group=GROUP     Print the value of the specified capturing group,");
+	    out.println("                        instead of the whole matching line");
+	    out.println("  -o, --only-matching   Print only the part of a matching line that matches,");
+	    out.println("                        equivalent to --group=0");
+	    out.println("  -h, --help");
+	}
+
+    protected boolean invert;
 	protected Pattern regex;
 	protected List<String> files;
 	protected Integer field;

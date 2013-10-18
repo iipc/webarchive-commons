7d4fbf451ed3ed8e09b0eb077b95411928a98a3c
Ilya Kreymer <ilya@archive.org>
2013-10-18 13:10:58 -0700

    FIX: MultiCDXInputSource: add optimization for output lazy initing of sequential cluster load

--- src/main/java/org/archive/format/cdx/MultiCDXInputSource.java
+++ src/main/java/org/archive/format/cdx/MultiCDXInputSource.java
@@ -75,14 +75,90 @@ public class MultiCDXInputSource implements CDXInputSource {
 		return scitr;
 	}
 	
+	// A special iterator which initializes on actual first use
+	protected static class LazyInitIterator implements CloseableIterator<String>
+	{
+		CDXInputSource source;
+		CloseableIterator<String> iter;
+		boolean failed = false;
+		
+		String key, start, end;
+		ZipNumParams params;
+		
+		protected LazyInitIterator(CDXInputSource source, String key, String start, String end, ZipNumParams params)
+		{
+			this.key = key;
+			this.start = start;
+			this.end = end;
+			
+			this.params = params;
+			
+			this.source = source;
+		}
+		
+		protected void initIter()
+		{
+			if (iter != null) {
+				return;
+			}
+			
+			try {
+	            iter = source.getCDXIterator(key, start, end, params);
+            } catch (IOException io) {
+				LOGGER.warning(io.toString());
+				iter = null;
+            }
+		}
+
+		@Override
+        public boolean hasNext() {
+			initIter();
+			
+			if (iter == null) {
+				return false;
+			}
+			
+			return iter.hasNext();
+        }
+
+		@Override
+        public String next() {
+			initIter();
+			
+			if (iter == null) {
+				return null;
+			}
+			
+			return iter.next();
+        }
+
+		@Override
+        public void remove() {
+
+        }
+
+		@Override
+        public void close() throws IOException {
+			if (iter != null) {
+				iter.close();
+			}
+        }		
+	}
+	
 	public CloseableIterator<String> createSeqIterator(String key, String start, String end, ZipNumParams params)
 	{
 		CloseableCompositeIterator<String> composite = new CloseableCompositeIterator<String>();
 		CloseableIterator<String> iter = null;
 		
-		for (CDXInputSource cdxReader : cdx) {
+		for (int i = 0; i < cdx.size(); i++) {
 			try {
-				iter = cdxReader.getCDXIterator(key, start, end, params);
+				CDXInputSource cdxReader = cdx.get(i);
+				
+				if (i == (cdx.size() - 1)) {
+					iter = cdxReader.getCDXIterator(key, start, end, params);
+				} else {
+					iter = new LazyInitIterator(cdxReader, key, start, end, params);
+				}
 				
 				if (!params.isReverse()) {
 					composite.addLast(iter);

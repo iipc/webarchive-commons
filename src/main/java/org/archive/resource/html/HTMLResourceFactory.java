5f223d60c365a53533b2ad7217deaa65b3a91667
Sebastian Nagel <snagel@apache.org>
2016-11-24 11:51:10 +0100

    Use CharsetDetector to guess encoding of HTML document

--- src/main/java/org/archive/resource/html/HTMLResourceFactory.java
+++ src/main/java/org/archive/resource/html/HTMLResourceFactory.java
@@ -1,9 +1,14 @@
 package org.archive.resource.html;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
+import org.archive.format.http.HttpHeaders;
+import org.archive.format.json.JSONUtils;
+import org.archive.format.text.charset.CharsetDetector;
+import org.archive.format.text.charset.StandardCharsetDetector;
 import org.archive.format.text.html.CDATALexer;
 import org.archive.format.text.html.LexParser;
 import org.archive.resource.MetaData;
@@ -13,8 +18,16 @@ import org.archive.resource.ResourceFactory;
 import org.archive.resource.ResourceParseException;
 import org.htmlparser.lexer.Page;
 import org.htmlparser.util.ParserException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HTMLResourceFactory implements ResourceFactory {
+
+	protected static final int CHARSET_GUESS_CHUNK_SIZE = 8192;
+	protected static final String HTTP_HEADER_PATH = "Envelope.Payload-Metadata.HTTP-Response-Metadata.Headers";
+
+	protected CharsetDetector charSetDetector = new StandardCharsetDetector();
+
 
 	public Resource getResource(InputStream is, MetaData parentMetaData,
 			ResourceContainer container) throws ResourceParseException, IOException {
@@ -22,8 +35,23 @@ public class HTMLResourceFactory implements ResourceFactory {
 		ExtractingParseObserver epo = new ExtractingParseObserver(hmd);
 		LexParser parser = new LexParser(epo);
 		CDATALexer lex = new CDATALexer();
-		// TODO: figure out charset:
-		String charset = "UTF-8";
+
+		// guess charset based on HTTP header and sniffed content chunk
+		is = new BufferedInputStream(is, CHARSET_GUESS_CHUNK_SIZE);
+		byte[] chunk = new byte[CHARSET_GUESS_CHUNK_SIZE];
+		is.mark(0);
+		int chunkSize = is.read(chunk, 0, CHARSET_GUESS_CHUNK_SIZE);
+		is.reset();
+		JSONObject headers = JSONUtils.extractObject(hmd.getTopMetaData(), HTTP_HEADER_PATH);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		if (headers.has("Content-Type")) {
+			try {
+				httpHeaders.add("Content-Type", headers.getString("Content-Type"));
+			} catch (JSONException e) { }
+		}
+
+		String charset = charSetDetector.getCharset(chunk, chunkSize, httpHeaders);
+
 		Page page;
 		try {
 			page = new Page(is, charset);

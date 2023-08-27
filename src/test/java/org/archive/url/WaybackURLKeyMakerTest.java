5161306d9ec993d1986f0d092c056f33ba3abdfe
Tom Morris <tfmorris@gmail.com>
2023-08-27 13:01:19 -0400

    Add non-UTF-8 encoded test from mailing list

--- src/test/java/org/archive/url/WaybackURLKeyMakerTest.java
+++ src/test/java/org/archive/url/WaybackURLKeyMakerTest.java
@@ -30,6 +30,8 @@ public class WaybackURLKeyMakerTest extends TestCase {
 				km.makeKey("http://1kr.ua/newslist.html?tag=%E4%EE%F8%EA%EE%EB%FC%ED%EE%E5"));
 		assertEquals("com,aluroba)/tags/%c3%ce%ca%c7%d1%e5%c7.htm",
 				km.makeKey("http://www.aluroba.com/tags/%C3%CE%CA%C7%D1%E5%C7.htm"));
+		assertEquals("ac,insbase)/xoops2/modules/xpwiki?%a4%d5%a4%af%a4%aa%a4%ab%b8%a9%a4%aa%a4%aa%a4%ce%a4%b8%a4%e7%a4%a6%bb%d4",
+			km.makeKey("https://www.insbase.ac/xoops2/modules/xpwiki/?%A4%D5%A4%AF%A4%AA%A4%AB%B8%A9%A4%AA%A4%AA%A4%CE%A4%B8%A4%E7%A4%A6%BB%D4"));
 	}
 
 }
